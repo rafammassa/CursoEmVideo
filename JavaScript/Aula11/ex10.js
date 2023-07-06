72460116377ef5c91e82959b535a0890231b22ba
Rafaella Massa <rafaella_massa@hotmail.com>
2023-07-06 15:06:13 -0300

    Estrutura if simples e composto

---
+++ JavaScript/Aula11/ex10.js
@@ -0,0 +1,15 @@
+var vel = 40.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){ //condição simples
+    console.log('MULTADO')
+}
+console.log('Dirija sempre com cinto de segurança!')
+
+var pais = 'BRASIL'
+
+if (pais == 'BRASIL'){ //condição composta
+    console.log('Brasileiro.')
+} else {
+    console.log('Estrangeiro')
+}
