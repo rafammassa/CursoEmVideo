3e7438983252dd56848694fd8251cc311e02050f
Rafaella Massa <rafaella_massa@hotmail.com>
2023-07-12 11:17:37 -0300

    Ambiente de teste while e do-while

---
+++ JavaScript/Aula13/ambiente.js
@@ -0,0 +1,13 @@
+var c = 0
+while (c < 3){
+    console.log(`WHILE - Passo ${c}`)
+    c++  
+}
+
+var cont = 0
+do {
+    console.log(`DO WHILE - Passo ${cont}`)
+    cont++     
+} while (cont < 3)
+
+
