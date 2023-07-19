2a026b4e1d4777e979303b33f538f3f28a9d8791
Rafaella Massa <rafaella_massa@hotmail.com>
2023-07-19 12:37:49 -0300

    Finalizando exercício Analisador de Números

--- JavaScript/Aula16EX/ex19/script.js
+++ JavaScript/Aula16EX/ex19/script.js
@@ -21,8 +21,43 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ` `
     } else {
         window.alert(`Valor inválido ou já encontrado na lista.`)
+    }
+
+    num.value = ` `
+    num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert(`Adicione valores antes de finalizar.`)
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let posicao in valores){
+            if (valores[posicao] > maior){
+                maior = valores[posicao]
+            }
+            if (valores[posicao] < menor){
+                menor = valores[posicao]
+            }
+            soma += valores[posicao]
+            media = soma / total
+        }
+        res.innerHTML = ` `
+        res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior número digitado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor número digitado foi ${menor}.</p>`
+        res.innerHTML += `<p>A soma dos valores digitados é ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
